ab9da3ca576e13009eb2686c8863df511076d629
Hannah Hafner <hhafner9@gmail.com>
2025-07-01 14:38:12 -0400

    Add: sample SBATCH scripts

---
+++ user_configs_examples/sample_generate_sbatch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# specifies sbatch resources needed for the job
+
+#SBATCH --mem=16G
+#SBATCH -J ddm_generate_sbatch
+#SBATCH --time=24:00:00
+#SBATCH --output=ddm_generate_sbatch.out
+#SBATCH --error=ddm_generate_sbatch.err
+#SBATCH --array=1-1
+
+# Your commands here
+
+module load python
+module load gcc
+
+python -m uv run generate --config-path /path/to/config_data_generation.yaml --log-level WARNING --output my_generated_data
---
+++ user_configs_examples/sample_jaxtrain_sbatch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# specifies sbatch resources needed for the job
+
+#SBATCH --mem=16G
+#SBATCH -J ddm_jaxtrain_sbatch
+#SBATCH --time=24:00:00
+#SBATCH --output=ddm_jaxtrain_sbatch.out
+#SBATCH --error=ddm_jaxtrain_sbatch.err
+#SBATCH --array=1-1
+
+# Your commands here
+
+module load python
+module load gcc
+
+python -m uv run jaxtrain --config-path /path/to/config_network_train.yaml --log-level WARNING --networks-path-base my_trained_network --training-data-folder my_generated_data/data/training_data/lan/training_data_n_samples_2000_dt_0.001/ddm --network-id 0 --dl-workers 2
